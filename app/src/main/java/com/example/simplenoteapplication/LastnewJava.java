185a3dd7cc32810f3ebb27454760054f7cc1c187
unknown <asifuddaulaasif@gmail.com>
2021-11-13 22:12:37 +0600

    last new java class added

---
+++ app/src/main/java/com/example/simplenoteapplication/LastnewJava.java
@@ -0,0 +1,11 @@
+package com.example.simplenoteapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class LastnewJava {
+    public LastnewJava() {
+    }
+
+    
+}
